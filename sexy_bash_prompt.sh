7f372a1c927716d32da1df6d70620e6ac4373762
Emanuel Campolo <emacampolo@gmail.com>
2017-07-07 09:40:06 -0300

    Replaced prompt with sexy_bash_prompt lib

---
+++ sexy_bash_prompt.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+(cd /tmp && git clone --depth 1 --config core.autocrlf=false https://github.com/twolfson/sexy-bash-prompt && cd sexy-bash-prompt && make install) && source ~/.bashrc && cd -
+
