b80042ebdbe8adaca782157fb434fbc556ee7a99
Emanuel Campolo <emacampolo@gmail.com>
2018-10-01 21:16:28 -0300

    Add bash prompt installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+set -e
 
 cd "$(dirname "${BASH_SOURCE}")";
 
+echo "Pulling last version from github"
 git pull origin master;
+echo "Downloading last sexy bash prompt"
+curl -sO https://raw.githubusercontent.com/twolfson/sexy-bash-prompt/master/.bash_prompt
 
 function doIt() {
 	rsync --exclude ".git/" \
--- sexy_bash_prompt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-(cd /tmp && git clone --depth 1 --config core.autocrlf=false https://github.com/twolfson/sexy-bash-prompt && cd sexy-bash-prompt && make install) && source ~/.bashrc && cd -
-
