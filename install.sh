74d8ca992813e4fba24a207e034a964a1a6366e6
Emanuel Campolo <emanuel.campolo@mercadolibre.com>
2018-12-15 20:36:22 -0300

    add install.sh and sync.sh

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install grep --with-default-names
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install Bash 4.
+brew install bash
+brew install bash-completion@2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+brew install screen
+brew install tree
+brew cask install iterm2
+brew install vim --with-override-system-vi
+
+# Remove outdated versions from the cellar.
+brew cleanup
+
+printf "Downloading sexy bash prompt\n"
+curl -sO https://raw.githubusercontent.com/twolfson/sexy-bash-prompt/master/.bash_prompt
+
+printf "Installing Vimplug\n"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugUpgrade +PlugUpdate +qall
+
+printf "Finished\n"
--- sync.sh
+++ sync.sh
@@ -5,14 +5,13 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 echo "Pulling last version from github"
 git pull origin master;
-echo "Downloading last sexy bash prompt"
-curl -sO https://raw.githubusercontent.com/twolfson/sexy-bash-prompt/master/.bash_prompt
 
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
+		--exclude "sync.sh" \
+		--exclude "install.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
